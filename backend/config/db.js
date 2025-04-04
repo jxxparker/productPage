b63bc8ff7b12b5f4611d982e1f33108cebd01742
jihun <jxxparker@gmail.com>
2025-04-04 17:43:01 -0500

    pushing

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,24 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    // console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
   }
 };
+
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     console.log("CONNECTING USING:", process.env.MONGO_URI); // 👈
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(`❌ MongoDB Connection Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// //password tjMkTGKkTvkxpnox
